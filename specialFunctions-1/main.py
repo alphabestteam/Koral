be5db1a0cd14a39712ca42a20670240b34889d39
koral <koral8484@gmail.com>
2023-09-11 11:25:24 +0300

    fixed

--- specialFunctions-1/main.py
+++ specialFunctions-1/main.py
@@ -4,10 +4,14 @@ def main():
     obj1 = Point(4, 8)
     obj2 = Point(4, 8)
 
-    print(obj1 == obj2)
-    print(obj1)
-    print(obj2)
-    print(obj1 + obj2)
+    print(obj1 == obj2) ## checks if the memory address is the same
+    
+    print(obj1) ## prints the memory address
+    
+    print(obj2) ## prints the memory address
+    
+    print(obj1 + obj2) # TypeError Exception , can't compare 2 objects
+
 
 
 if __name__ == "__main__":
