f0dcdb46a0f011f0653d18a33739129ba37fc46d
Koraleinav <102703114+Koraleinav@users.noreply.github.com>
2023-09-18 09:35:13 +0300

    Delete bank_supermarket/test.py
    yes

--- bank_supermarket/test.py
+++
@@ -1,7 +0,0 @@
-import os
-def main():
-    os.makedirs(os.path.join("./costumers"),exist_ok=True)
-    print(len([name for name in os.listdir('./costumers/')]))
-    
-if __name__ == "__main__":
-    main()
