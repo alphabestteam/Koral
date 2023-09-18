f323561f04f4110aa6ae31fd82e699008d7795a5
koral <koral8484@gmail.com>
2023-09-18 09:17:00 +0300

    chenged api connection function

--- bank/api.py
+++ bank/api.py
@@ -5,6 +5,17 @@ PORT = 12347
 MEMORY = 2048
 
 class Api:
+    def connect_to_server(self, ip: int, port: int, action: str, account_name, account_number = "0" , account_balance = "0", amount_of_money_to_deposit = "0", other_account_name = "0", other_account_number = "0"):
+        my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        my_socket.connect((ip, port))
+        if action == "create_account":
+            self.create_account(account_name, account_balance)
+        elif action == "deposit_money":
+            self.deposit_money(account_name, account_number, amount_of_money_to_deposit)
+        elif action == "withdraw_money":
+            self.withdraw_money(account_name, account_number, amount_of_money_to_deposit)
+        elif action == "transfer_money":
+            self.transfer_money(account_name, account_number, amount_of_money_to_deposit, other_account_name, other_account_number)
 
     def create_account(self, account_name, account_balance):
             my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
