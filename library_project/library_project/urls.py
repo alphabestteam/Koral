c9d7b6c29e87a93ed2423dd8891207344c9b263b
koral <koral8484@gmail.com>
2023-10-23 18:02:36 +0300

    ffinished

--- library_project/library_project/urls.py
+++ library_project/library_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/books/', include('books.url'))
+    path('api/books/', include('books.urls'))
 ]
