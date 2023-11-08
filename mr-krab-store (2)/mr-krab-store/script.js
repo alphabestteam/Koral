856c58d2b4648d697bb67ddb33c3266573e34ee9
koral <koral8484@gmail.com>
2023-11-08 14:39:26 +0200

    changing gif after 5 seconds

--- mr-krab-store (2)/mr-krab-store/script.js
+++ mr-krab-store (2)/mr-krab-store/script.js
@@ -1,4 +1,3 @@
-
 /*
 List of endpoints:
   GET - http://localhost:8000/hello -> {'Hello': 'World'} Here as an example
@@ -7,3 +6,41 @@ List of endpoints:
   POST - http://localhost:8000/orders -> An endpoint to handle an order. The order is in the http body as so: { 'items': items }
 
 */
+
+menu = document.getElementById("menu");
+productName = document.createElement("p")
+// productName = 
+//to show the gif : 
+
+
+
+
+
+
+
+
+
+function fetchMenu() {
+  document.getElementById("loader").style.display = "block";
+
+  fetch("http://localhost:8000/menu")
+      .then(response => {
+          if (!response.ok) {
+              throw new Error("Couldn't Fetch");
+          }
+          return response.text();
+      })
+      .then(data => {
+          document.getElementById("loader").style.display = "none";
+
+          document.getElementById("menu").innerHTML = data;
+      })
+      .catch(error => {
+          console.error("Fetch error:", error);
+      });
+}
+
+setTimeout(fetchMenu, 5000);
+
+
+
