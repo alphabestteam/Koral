49f659532d31648eaa374927c615f7e5adbd3928
koral <koral8484@gmail.com>
2023-11-20 16:44:22 +0200

    finished

--- basics-assignment-2-start/basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/basics-assignment-2-start/src/app/app.component.ts
@@ -6,8 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  text = document.getElementById("updatePara")
-  updatePara(event: Event){
 
+  textInput: string = '';
+  textOutput: string = '';
+  buttonUser = document.getElementById("buttonUser");
+
+
+  updatePara(event: Event){
+    this.textOutput = this.textInput;
+  }
+  
+  restartPara(){
+    this.textInput = "";
+    this.textOutput = "";
+  }
+
+  checkInputLength(){
+    if (this.textInput) {
+      if (this.textInput.length === 0) {
+        this.buttonUser.style.cursor = "not-allowed";
+      } else {
+        this.buttonUser.style.cursor = "pointer";
+      }
+    }
   }
 }
