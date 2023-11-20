4a6db94303dee38846e2de24b8413132f832e7cf
koral <koral8484@gmail.com>
2023-11-20 18:56:50 +0200

    finished

--- basics-assignment-3-start/basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/basics-assignment-3-start/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { count } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-}
+
+
+  // for bootstrap : 
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+
+
+  counter: number = 0;
+    details: boolean = true;
+    counterArray: number[] = [];
+
+    displayDetails() {
+      this.counter++;
+      this.counterArray.push(this.counter);
+      this.details = !this.details;
+    };
+
+    getHidden() {
+      return this.details ? 'visible' : 'hidden';
+    }
+  }
+
+
