9c1a05e3200a6bc9eceff739f77f242ab9788c6b
koral <koral8484@gmail.com>
2023-11-21 16:43:37 +0200

    finished

--- pdf6/pdf6/src/app/app.component.ts
+++ pdf6/pdf6/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'pdf6';
+  profileForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.profileForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    })
+  }
+
+  onSubmit() {
+    if (this.profileForm.valid) {
+      console.log(this.profileForm.value);
+    }
+  }
 }
