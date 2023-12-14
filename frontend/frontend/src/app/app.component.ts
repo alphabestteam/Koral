3bfd0bd2aa9fd4898e0c3c1e95ac9b6155237181
koral <koral8484@gmail.com>
2023-12-14 10:41:29 +0200

    initial - finished main html

---
+++ frontend/frontend/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  showLoader: boolean = true;
+  showMainContent: boolean = false;
+
+  ngOnInit() {
+    // Simulate loading delay before showing main content
+    setTimeout(() => {
+      this.showLoader = false;
+      setTimeout(() => {
+        this.showMainContent = true; 
+      }, );
+    }, 3000); // Show main content after 3 seconds
+  }
+}
